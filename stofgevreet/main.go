312f23ebc6e99d24e451ec891ae186d2c8d92b26
Hendrik Jacobus Barnard <heinrichbarnard@hotmail.com>
2020-11-15 09:26:29 +0200

    Moved stofgevreet

--- stofgevreet/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/Mallekoppie/goslow/platform"
-)
-
-func main() {
-	platform.StartHttpServer(Routes)
-}
--- stofgevreet/model/config.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Config struct {
-	Name string
-	Port string
-}
